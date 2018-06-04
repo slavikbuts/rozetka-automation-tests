34fddcdd797fc7bf70f61b5f7bcc929e0c945bba
YaroslavButs <yaroslav_buts@epam.com>
2018-06-04 16:35:00 +0300

    update assertion

--- test/homepage.js
+++ test/homepage.js
@@ -1,9 +1,9 @@
-const {expect} = require('chai');
+const { expect } = require('chai');
 
-describe('Rozetka home page', function() {
+describe('Rozetka home page', function () {
     it('should have the right title - the fancy generator way', function () {
         browser.url('/ua');
         var title = browser.getTitle();
-        assert.equal(title, 'Интернет-магазин ROZETKA™: фототехника, видеотехника, аудиотехника, компьютеры и компьютерные комплектующие');
+        expect(title).to.be.equal('Интернет-магазин ROZETKA™: фототехника, видеотехника, аудиотехника, компьютеры и компьютерные комплектующие');
     });
 });
