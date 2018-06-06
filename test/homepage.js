5569121410f044c3854cdf42bd8bd676864793d0
YaroslavButs <yaroslav_buts@epam.com>
2018-06-06 16:44:36 +0300

    add 'log in' test

--- test/homepage.js
+++ test/homepage.js
@@ -1,9 +1,18 @@
 const { expect } = require('chai');
 
 describe('Rozetka home page', function () {
-    it('should have the right title - the fancy generator way', function () {
+    it('should have the right title', function () {
         browser.url('/ua');
-        var title = browser.getTitle();
+        const  title = browser.getTitle();
         expect(title).to.be.equal('Интернет-магазин ROZETKA™: фототехника, видеотехника, аудиотехника, компьютеры и компьютерные комплектующие');
+    });
+    it('log in', function () {
+        browser.url('/ua');
+        const  title = browser.getTitle();
+        expect(title).to.be.equal('Интернет-магазин ROZETKA™: фототехника, видеотехника, аудиотехника, компьютеры и компьютерные комплектующие');
+        browser.click('[data-lang="ua"]')
+        browser.click('#header_user_menu_parent')
+        const authTitle = browser.getText('[class="auth-title"]')
+        expect(authTitle).to.be.equal('Вхід в інтернет-магазин');
     });
 });
