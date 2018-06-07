7e8065924ef4e0687ba74fb83177ed0a1d1e1023
YaroslavButs <yaroslav_buts@epam.com>
2018-06-07 17:54:44 +0300

    update tests

--- test/homepage.js
+++ test/homepage.js
@@ -1,18 +1,29 @@
 const { expect } = require('chai');
 
 describe('Rozetka home page', function () {
+    beforeEach(function () {
+        browser.url('/ua')
+        browser.windowHandleFullscreen();
+    })
     it('should have the right title', function () {
-        browser.url('/ua');
-        const  title = browser.getTitle();
+        const title = browser.getTitle();
         expect(title).to.be.equal('Интернет-магазин ROZETKA™: фототехника, видеотехника, аудиотехника, компьютеры и компьютерные комплектующие');
     });
     it('log in', function () {
-        browser.url('/ua');
-        const  title = browser.getTitle();
+        const title = browser.getTitle();
+
         expect(title).to.be.equal('Интернет-магазин ROZETKA™: фототехника, видеотехника, аудиотехника, компьютеры и компьютерные комплектующие');
-        browser.click('[data-lang="ua"]')
-        browser.click('#header_user_menu_parent')
+
+        browser.click('[class="header-user-title"]')
         const authTitle = browser.getText('[class="auth-title"]')
-        expect(authTitle).to.be.equal('Вхід в інтернет-магазин');
+        if (!authTitle == 'Вход в интернет-магазин') {
+            expect(authTitle).to.be.equal('Вхід в інтернет-магазин');
+        }
+        browser.setValue('[name="login"]', 'test@gmail.com');
+        browser.setValue('div[name="simple_auth"] .auth-input-text', 'qwerqwerqwerqwerqwe');
+        browser.click('[class="btn-link-i"]')
+        // const wrongPassTitle = browser.getText('auth-forgot-pass-tooltip-note');
+        // console.log('>>>>>>>>>', wrongPassTitle);
+        // expect(wrongPassTitle).to.be.equal('Перевірте розкладку клавіатури і Caps Lock')
     });
 });
